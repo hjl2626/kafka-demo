48c991e9aaa9b17a3f9718e88b6564d742309f49
hjl <huangjunlun@iot.chinamobile.com>
2016-08-16 11:52:30 +0800

    modify consumer-thread --synchronized ++AtomicLong modify pom.xml ++kafka-clients 依赖

--- src/main/java/com/kafka/consumer/thread/ConsumerThread.java
+++ src/main/java/com/kafka/consumer/thread/ConsumerThread.java
@@ -8,7 +8,8 @@ import kafka.consumer.KafkaStream;
 import kafka.message.MessageAndMetadata;
 import org.apache.log4j.Logger;
 import org.bson.Document;
-import scala.collection.mutable.SynchronizedSet;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 消息消费线程
@@ -22,7 +23,7 @@ public class ConsumerThread implements Runnable {
 
     private  MongoCollection<Document> coll = MongoDBUtil.instance.getCollection("mongodb-test", "logs");
 
-    private static int index = 0;
+    private static AtomicLong index = new AtomicLong();
 
 
     private static Logger log = Logger.getLogger(ConsumerThread.class);
@@ -59,15 +60,13 @@ public class ConsumerThread implements Runnable {
 
             String messageT = new String(message.message());
             log.info(Thread.currentThread().getName() + "  接收到: " + messageT + "来自于topic：[" + topic + "] + 第partition[  " + partition + "  ]" + "   key = " + message.key());
-            synchronized(ConsumerThread.class) {
                 Document bson = new Document();
-                bson.put("id", ConsumerThread.index++);
+                bson.put("id", ConsumerThread.index.getAndIncrement());
                 bson.put("content", messageT);
                 coll.insertOne(bson);
             /*
              * 这里需要选择一种 "合适的存储方案"
              * */
-            }
         }
     }
 
